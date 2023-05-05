ae084e203a981186ebe95f3c084599ab0b052325
Петр Строителев <drpepper-str@yandex.ru>
2023-05-05 21:47:40 +0900

    end_version_1

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    objects = None
     post_title = models.CharField(max_length=30)
     post_date = models.DateTimeField()
     post_text = models.CharField(max_length=300)
     post_image = models.ImageField(upload_to="post_images/")
+
+    def get_summary (self):
+        return self.post_text[:70]
+
+    def __str__(self):
+        return self.post_title
